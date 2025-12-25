e0990ac5b773a158d2dd82e6af0fa728f2bdbed5
douglas-wesley <douglaswesley0407@gmail.com>
2025-12-25 15:47:28 -0300

    feat: implement service interfaces for Conta and Transacao with method overrides

--- src/main/java/com/example/Gerenciamento_Financeiro/services/TransacaoServices.java
+++ src/main/java/com/example/Gerenciamento_Financeiro/services/TransacaoServices.java
@@ -5,6 +5,7 @@ import com.example.Gerenciamento_Financeiro.dto.TransacaoResponseDTO;
 import com.example.Gerenciamento_Financeiro.model.Transacao;
 import com.example.Gerenciamento_Financeiro.model.enums.Tipo;
 import com.example.Gerenciamento_Financeiro.repository.TransacaoRepository;
+import com.example.Gerenciamento_Financeiro.services.interfaces.ITransacaoServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,24 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Service
-public class TransacaoServices {
+public class TransacaoServices implements ITransacaoServices {
 
     @Autowired
     private TransacaoRepository repository;
 
+    @Override
     public TransacaoResponseDTO criaTransacao(TransacaoRequestDTO dto) { // ajustar as regras de negocio aqui
         if (dto.getValor() == null || dto.getValor().compareTo(BigDecimal.ZERO) <= 0.0){
             throw new IllegalArgumentException("O valor da transação deve ser maior que zero.");
         };
 
-        if (dto.getData() == null || dto.getData().isAfter(LocalDate.now())){
-            throw new IllegalArgumentException("A data da transação não pode ser no futuro.");
+        if (dto.getData() == null){
+            throw new IllegalArgumentException("A data da transação não pode ser nula.");
         };
 
         if (dto.getTipo() == null || (dto.getTipo() != Tipo.RECEITA && dto.getTipo() != Tipo.DESPESA)){
             throw new IllegalArgumentException("Tipo de transação inválido.");
         };
-
         Transacao transacao = new Transacao();
         transacao.setDescricao(dto.getDescricao());
         transacao.setValor(dto.getValor());
@@ -47,6 +48,7 @@ public class TransacaoServices {
         );
     }
 
+    @Override
     public void deletaTransacao(Long id) {
         if (!repository.existsById(id)){
             throw new IllegalArgumentException("Transação não existe."); // Ajustar o status para 404
@@ -57,6 +59,7 @@ public class TransacaoServices {
         }
     }
 
+    @Override
     public TransacaoResponseDTO getTransacaoById(Long id){ // Verificar
         Transacao transacao = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Transação não encontrada."));
         return new TransacaoResponseDTO(transacao.getId(),
@@ -66,7 +69,11 @@ public class TransacaoServices {
                 transacao.getTipo().toString());
     }
 
-    public Transacao updateTransacao(Long id, TransacaoRequestDTO dto){
+    // Adicionar um get para todos as transações
+
+    // Ajustar isso
+    @Override
+    public TransacaoResponseDTO updateTransacao(Long id, TransacaoRequestDTO dto){
         Transacao transacao = repository.findById(id).orElseThrow( () -> new IllegalArgumentException("Transação não encontrada."));
 
         transacao.setData(dto.getData());
@@ -74,6 +81,14 @@ public class TransacaoServices {
         transacao.setTipo(dto.getTipo());
         transacao.setValor(dto.getValor());
 
-        return repository.save(transacao);
+        repository.save(transacao);
+
+        return new TransacaoResponseDTO(
+                transacao.getId(),
+                transacao.getDescricao(),
+                transacao.getValor(),
+                transacao.getData(),
+                transacao.getTipo().toString()
+        );
     }
 }
