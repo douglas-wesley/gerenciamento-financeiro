ba2a8efeeb8844f87dad29d1341ac5bc37bc1583
douglas-wesley <douglaswesley0407@gmail.com>
2025-12-25 19:35:04 -0300

    feat: add contaId and categoriasIds fields to TransacaoRequestDTO for enhanced transaction management

--- src/main/java/com/example/Gerenciamento_Financeiro/dto/TransacaoRequestDTO.java
+++ src/main/java/com/example/Gerenciamento_Financeiro/dto/TransacaoRequestDTO.java
@@ -1,11 +1,14 @@
 package com.example.Gerenciamento_Financeiro.dto;
 
+import com.example.Gerenciamento_Financeiro.model.Conta;
 import com.example.Gerenciamento_Financeiro.model.enums.Tipo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Schema(description = "DTO para requisições de transações")
@@ -15,4 +18,9 @@ public class TransacaoRequestDTO {
     private BigDecimal valor;
     private LocalDate data;
     private Tipo tipo;
+    private Long contaId;
+
+    private List<Long> categoriasIds;
+
+    // Adicionar Categoria
 }
